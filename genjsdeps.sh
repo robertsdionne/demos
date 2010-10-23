fdc77301855359965b7cae5d467bcb4ede0bebe0
Robert Dionne <robertsdionne@gmail.com>
2010-10-23 18:58:50 -0400

    Second.

---
+++ genjsdeps.sh
@@ -0,0 +1,6 @@
+# Copyright 2010 Robert Scott Dionne. All Rights Reserved.
+#
+# Generates Javascript dependencies.
+../closure-library/closure/bin/build/depswriter.py \
+  --root_with_prefix=". ../../../astoroids/" \
+  --output_file=deps.js
